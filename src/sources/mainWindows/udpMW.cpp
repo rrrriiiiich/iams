14652a87a575cb30e087eb9fbaee96e77a9031bb
JinJieBeWater <cjj20030813@qq.com>
2024-12-20 03:10:14 +0800

    feat: 优化udpMW日志

--- src/sources/mainWindows/udpMW.cpp
+++ src/sources/mainWindows/udpMW.cpp
@@ -28,8 +28,12 @@ void udpMW::on_bindButton_clicked()
     // 版本一：绑定端口为 50000
     // udpsocket->bind(ui->portEdit->text().toInt());
     // // 版本二：指定ip地址和端口进行绑定
+    // 先解除绑定
+    udpsocket->close();
+    // 绑定
     udpsocket->bind(QHostAddress(ui->addressEdit->text()), ui->portEdit->text().toInt());
     SYSTEMLog() << "bind: " << ui->addressEdit->text() << ":" << ui->portEdit->text();
+    ui->recvPlainTextEdit->appendPlainText(QString("[%1:%2] %3").arg(ui->addressEdit->text()).arg(ui->portEdit->text()).arg("bind success"));
 }
 
 void udpMW::on_readData()
@@ -39,10 +43,29 @@ void udpMW::on_readData()
     datagram.resize(udpsocket->pendingDatagramSize());
     QHostAddress sender;
     quint16 senderPort;
-    udpsocket->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
-    SYSTEMLog() << "read: " << datagram;
-    // SYSTEMLog() << "read: " << datagram.data();
-    ui->recvPlainTextEdit->appendPlainText(sender.toString() + ":" + QString::number(senderPort) + ": " + datagram);
+
+    // 读取数据包
+    qint64 size = udpsocket->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
+
+    // 检查socket错误
+    if (udpsocket->error() != QAbstractSocket::UnknownSocketError)
+    {
+        QString errorMsg = QString("network error: %1").arg(udpsocket->errorString());
+        SYSTEMLog() << errorMsg;
+        ui->recvPlainTextEdit->appendPlainText(errorMsg);
+    }
+
+    // 检查数据包大小
+    if (size <= 0)
+    {
+        SYSTEMLog() << "size <= 0";
+    }
+
+    // 正常数据包处理
+    SYSTEMLog() << "receive from" << sender.toString() << ":" << senderPort;
+    SYSTEMLog() << "receive:" << datagram;
+    ui->recvPlainTextEdit->appendPlainText(
+        QString("[%1:%2] %3").arg(sender.toString()).arg(senderPort).arg(QString(datagram)));
 }
 
 void udpMW::on_sendButton_clicked()
@@ -50,4 +73,6 @@ void udpMW::on_sendButton_clicked()
     QByteArray datagram = ui->sendEdit->text().toUtf8();
     udpsocket->writeDatagram(datagram, datagram.size(), QHostAddress(ui->sendAdderssEdit->text()), ui->sendPortEdit->text().toInt());
     SYSTEMLog() << "send: " << datagram;
+
+    ui->recvPlainTextEdit->appendPlainText(QString("[send to %1:%2] %3").arg(ui->sendAdderssEdit->text()).arg(ui->sendPortEdit->text()).arg(QString(datagram)));
 }
