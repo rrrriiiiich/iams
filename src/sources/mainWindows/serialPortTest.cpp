566a7dc5ecfebf285be17b02eefac59cfa3bbea4
JinJieBeWater <cjj20030813@qq.com>
2024-12-24 15:46:08 +0800

    feat: 添加串口设备检测

--- src/sources/mainWindows/serialPortTest.cpp
+++ src/sources/mainWindows/serialPortTest.cpp
@@ -6,8 +6,17 @@ serialPortTest::serialPortTest(QWidget *parent) : QMainWindow(parent),
 {
     ui->setupUi(this);
 
+    if (!QFile::exists("/dev/ttySAC2"))
+    {
+        Log() << "串口设备 /dev/ttySAC2 不存在";
+        QMessageBox::information(this, "提示", "串口设备不存在");
+        return;
+    }
+
     // 创建串口对象，设置串口设备为/dev/ttySAC0
     serialPort = new QSerialPort("/dev/ttySAC2", this);
+    // 设置串口名称为/dev/ttySAC0
+    serialPort->setPortName("/dev/ttySAC2");
     // 设置波特率为115200
     serialPort->setBaudRate(QSerialPort::Baud115200);
     // 设置数据位为8位
@@ -28,7 +37,7 @@ serialPortTest::serialPortTest(QWidget *parent) : QMainWindow(parent),
     else
     {
         Log() << "open failed";
-        QMessageBox::warning(this, "提示", "串口打开失败");
+        QMessageBox::information(this, "提示", "串口打开失败");
     }
 
     connect(serialPort, &QSerialPort::readyRead, this, &serialPortTest::on_readyRead);
@@ -36,9 +45,17 @@ serialPortTest::serialPortTest(QWidget *parent) : QMainWindow(parent),
 
 void serialPortTest::on_readyRead()
 {
+    // 读取串口数据
+    QByteArray data = serialPort->readAll();
+    // 将数据转换为QString
+    QString str = QString(data);
+    Log() << serialPort->portName() << ": " << str;
+    ui->recvPlainTextEdit->appendPlainText(serialPort->portName() + ": " + str);
 }
 
 serialPortTest::~serialPortTest()
 {
     delete ui;
+    serialPort->close();
+    delete serialPort;
 }
